c8d9db3574257f34270fc93b2e9f4e8b8131cd71
Damir Zaripov <damir.zaripov1998@mail.ru>
2022-10-21 18:47:25 +0300

    Страница новостей

---
+++ src/components/Dialogs/News.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import './News.css';
+
+
+
+const News = () => {
+    return (
+        <div>
+            <div className='News-wrapper'>
+                <div className='News-block'>
+                    <div className='News'>
+                        <img src='https://www.sostav.ru/app/public/images/news/2013/11/20/original/top.jpg'></img>
+                    </div>
+                    <div className="info">
+                        Новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="button-wrap">
+                        <a className='btn' href=''>Подробнее</a>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div className='News-wrapper'>
+                <div className='News-block'>
+                    <div className='News'>
+                        <img src='https://img2.akspic.ru/previews/2/9/0/9/6/169092/169092-sipuha-neyasyt-ptica-klyuv-naturalnyj_material-500x.jpg'></img>
+                    </div>
+                    <div className="info">
+                        Новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="button-wrap">
+                        <a className='btn' href=''>Подробнее</a>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+            <div className='News-wrapper'>
+                <div className='News-block'>
+                    <div className='News'>
+                        <img src='https://rus-pic.ru/wp-content/uploads/2021/05/krasivye-kartinki-na-avu-40-foto-4ead6e3.jpg'></img>
+                    </div>
+                    <div className="info">
+                        Новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="info-text">
+                        Текст статьи или новости
+                    </div>
+                    <div className="button-wrap">
+                        <a className='btn' href=''>Подробнее</a>
+
+
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+
+    )
+}
+
+
+export default News;
